59dccf908e08c3a328a5482f7ce397110c8c4a52
tachikousai <139242820+tachikousai@users.noreply.github.com>
2023-07-13 12:57:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -11,9 +11,21 @@ function popup_window(title_text, message_html) {
       var main_text = document.getElementById("popup_message");
       main_text.innerHTML = message_html;
 
-      document.getElementById("popup_box").style.animation ="fadeIn 0.25s 1 forwards";
+      document.getElementById("popup_box").style.animation = "fadeIn 0.25s 1 forwards";
 }
 
 function close_window() {
-      document.getElementById("popup_box").style.animation ="fadeOut 0.25s 1 forwards";
+      document.getElementById("popup_box").style.animation = "fadeOut 0.25s 1 forwards";
 }
+
+$(window).on('load', function () {
+      let headerHeight = $('#header').outerHeight();
+      let speed = 600;
+      let href = location.hash;;
+      console.log(href);
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top - headerHeight;
+      console.log(target.offset().top);
+      console.log(headerHeight);
+      $('html, body').stop().animate({ scrollTop: position }, speed, "swing");
+});
